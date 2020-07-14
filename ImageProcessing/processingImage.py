70b14999e767109323a1fedf17b8809da6f7b4d1
Zackary <zspink@nea.k12.ar.us>
2020-07-13 21:51:58 -0500

    Update processingImage.py

--- ImageProcessing/processingImage.py
+++ ImageProcessing/processingImage.py
@@ -7,7 +7,7 @@ def main():
     chdir(dirPath)
 
     im = Image.open('lotus.png')
-    im.show()
+    im.show() # Doesn't work in repl.it. Save image to view it.
 
 if __name__ == '__main__':
     main()
