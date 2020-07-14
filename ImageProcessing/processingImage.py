1507368980f080d4d06f5d6b6d845b15d6172d8b
Moontok <zspink@nea.k12.ar.us>
2020-07-14 09:53:40 -0500

    s

--- ImageProcessing/processingImage.py
+++ ImageProcessing/processingImage.py
@@ -1,5 +1,5 @@
 from PIL import Image
-from os import chdir, path, getcwd
+from os import chdir, path
 
 def main():
     # Get the directory of this script.
@@ -7,7 +7,26 @@ def main():
     chdir(dirPath)
 
     im = Image.open('lotus.png')
-    im.show() # Doesn't work in repl.it. Save image to view it.
+    #im = im.rotate(90)
+    #im.save('lotus_rotated.png')
+
+    #pngToJpg(im)
+    im1 = Image.open('image2.png')
+    im2 = Image.open('image1.png')
+    im2 = im2.convert('RGBA')
+    mergeThee(im2, im1)
+
+def pngToJpg(image):
+    '''Convert a .png to a .jpg.'''
+    image = image.convert('RGB')
+    image.save('lotus.jpg')
+
+def mergeThee(image1, image2):
+    ''' Composite the second image onto the first
+    if the modes and size are the same.'''
+
+    mergedImage = Image.alpha_composite(image1, image2)
+    mergedImage.save('Cool_Image.png')
 
 if __name__ == '__main__':
     main()
